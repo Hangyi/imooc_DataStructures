da282bf30f7ac454ce5093d662bf9a2109b141e0
Hangyi <sweat0909@gmail.com>
2020-06-01 21:22:17 +0800

    新增循环队列

---
+++ Queue/src/LoopQueue.java
@@ -0,0 +1,131 @@
+import javax.xml.crypto.Data;
+
+/**
+ * @author ZhuHangYi
+ * @create 2020-06-01 20:31
+ */
+
+
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front;
+    private int tail;
+    private int size;
+
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        return data[front];
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(2 * getCapacity());
+        }
+
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+        }
+
+        E ret = data[front];
+        data[front] = null;
+
+        front = (front + 1) % data.length;
+        size--;
+
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
+        }
+
+        return ret;
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue: size = %d, capacity = %d\n", size, getCapacity()));
+        res.append("front [");
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            res.append(data[i]);
+            if ((i + 1) % data.length != tail) {
+                res.append(", ");
+            }
+        }
+
+        res.append("] tail");
+        return res.toString();
+    }
+
+
+    public static void main(String[] args) {
+        LoopQueue<Integer> queue = new LoopQueue<>();
+
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue.toString());
+            if(i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue.toString());
+            }
+        }
+
+        System.out.println(queue.isEmpty());
+
+        System.out.println("capacity = " + queue.getCapacity());
+
+        System.out.println("size = " + queue.getSize());
+
+        System.out.println(queue.getFront());
+
+
+    }
+}
